1c06aefa70ff6c2ba0547b76ca581722b4fb411e
Christian Steinmann <christian.steinmann77@gmail.com>
2016-10-22 21:41:06 +0200

    refactor(core): move user routes to user-routes.ts script

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { TokenService } from '../services/token-service';
 import { UserService } from '../services/user-service';
 import { KeyStoreService } from '../services/key-store-service';
-import { authRouteFactory } from './auth-route';
+import { authRoutesFactory } from './auth-routes';
 import { IApp } from '../server';
+import { userRoutesFactory } from './user-routes';
 
 export function routesFactory(app: IApp, tokenService: TokenService, userService: UserService, keyStoreService: KeyStoreService): void {
-    authRouteFactory(app, tokenService, userService, keyStoreService);
+    authRoutesFactory(app, tokenService, userService, keyStoreService);
+    userRoutesFactory(app, tokenService, userService, keyStoreService);
 }
--- src/server.ts
+++ src/server.ts
@@ -7,22 +7,13 @@
 import * as express from 'express';
 import { config as env, IConfig } from './config';
 import { Request } from '~express/lib/request';
-import { Response } from '~express/lib/response';
 import { UserService } from './services/user-service';
 import * as StrategyUtils from './utils/strategy-utils';
 import { KeyStoreService } from './services/key-store-service';
-import * as bodyParser from 'body-parser';
 import { ParsedAsJson } from 'body-parser';
 import { TokenService } from './services/token-service';
 import { KeyStoreResult } from './services/key-store-service';
 import { Application } from '~express/lib/application';
-import {
-    tokenExists, tokenVerifyCreator, userFromTokenCreator, checkUserParamCreator, IUserFromTokenRequest, ICheckUserFromParamRequest
-} from './middleware';
-import { isEmpty, pick } from 'lodash';
-import { IUserData } from './services/user-service';
-import { IUserModel } from './services/user-service';
-import { UserNotFoundError } from './errors';
 import { Server } from 'http';
 import { Profile } from 'passport-facebook';
 import { IHash } from './types';
@@ -47,16 +38,12 @@ export interface IStrategyOptions {
     keyStoryStrategyOptions?: IHash;
 }
 
-let server: Server;
-
 export interface IAuthResponse {
     token: string;
     refreshToken: string;
 }
 
-export type IRefreshRequest = IAppRequest & IUserFromTokenRequest;
-export type DeleteUserRequest = IAppRequest & ICheckUserFromParamRequest;
-export type PutUserRequest = IAppRequest & ICheckUserFromParamRequest;
+let server: Server;
 
 export async function stop(): Promise<void> {
     if (!server) {
@@ -81,71 +68,8 @@ export async function start(config?: IConfig & IStrategyOptions): Promise<Server
     const userService = new UserService(userStrategy);
     const keyStoreService = new KeyStoreService(keyStoreStrategy);
     const tokenService = new TokenService();
-    // Middleware
-    const bodyParserJson = bodyParser.json();
-    const tokenVerify = tokenVerifyCreator(tokenService, keyStoreService);
-    const userFromToken = userFromTokenCreator(userService);
-    const checkUserParam = checkUserParamCreator('id');
 
     routesFactory(app, tokenService, userService, keyStoreService);
-
-    app.delete('/user/:id', bodyParserJson, tokenExists, tokenVerify, userFromToken, checkUserParam, async(req: DeleteUserRequest, res: Response) => {
-        try {
-            let deleted: boolean;
-            if (config.TRUE_DELETE_ENABLED) {
-                deleted = await userService.deleteUser(req.userId);
-            } else {
-                console.log(await userService.updateUser(req.userId, {deleted: true}));
-                deleted = !!(await userService.updateUser(req.userId, {deleted: true}));
-            }
-
-            if (deleted) {
-                res.status(200);
-                res.end();
-            }
-        } catch (err) {
-            console.error(err);
-            res.status(500);
-            res.json({message: 'Service failed'});
-        }
-    });
-
-    app.put('/user/:id', bodyParserJson, tokenExists, tokenVerify, userFromToken, checkUserParam, async(req: PutUserRequest, res: Response) => {
-        try {
-            let data = req.body && req.body.data;
-            if (typeof data !== 'object' || isEmpty(data)) {
-                res.status(204);
-                res.end();
-                return;
-            }
-
-            const userData: IUserData = pick<IUserData, any>(data, ['username', 'email', 'isAdmin', 'password']);
-
-            if (!req.tokenUser.isAdmin) {
-                delete userData.isAdmin;
-            }
-
-            if (isEmpty(data)) {
-                res.status(204);
-                res.end();
-                return;
-            }
-
-            let updatedUser = await userService.updateUser(req.userId, userData);
-
-            res.status(200);
-            res.json(pick<IUserModel, IUserModel>(updatedUser, ['username', 'email', 'isAdmin', '_id']));
-        } catch (err) {
-            if (err instanceof UserNotFoundError) {
-                res.status(404);
-                res.send('Could not find user');
-                return;
-            }
-            console.error(err);
-            res.status(500);
-            res.json({message: 'Service failed'});
-        }
-    });
 
     app.keyStoreResult = await keyStoreService.initRsa();
 
